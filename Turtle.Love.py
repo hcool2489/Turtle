0460344823ce694b9b3c99d7196fb97b36d9ca32
Harshit <hcool2489@gmail.com>
2020-02-09 11:53:06 +0530

    Initial commit

---
+++ Turtle.Love.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.bgcolor('#b4e5d2')
+turtle.title('Turtle Love')
+turtle.pensize(2)
+def round():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+
+turtle.speed(0)
+turtle.color('black', 'black')
+turtle.left(140)
+turtle.forward(111.65)
+round()
+turtle.left(120)
+round()
+turtle.forward(111.65)
